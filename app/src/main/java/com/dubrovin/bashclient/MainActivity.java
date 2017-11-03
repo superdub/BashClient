5b72e9fe77f1b1c0f7d7814c056e27784e3c3fac
superdub <Super2002>
2017-11-03 18:45:46 +0300

    start new Project

---
+++ app/src/main/java/com/dubrovin/bashclient/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dubrovin.bashclient;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dubrovin on 03.11.17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
